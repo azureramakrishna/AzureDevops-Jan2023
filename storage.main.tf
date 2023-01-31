085af575e55506e0261cae7c096afc788e9bf143
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2023-01-31 14:11:33 +0530

    Create storage.main.tf

---
+++ storage.main.tf
@@ -0,0 +1,29 @@
+# AzureRM provider version
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.30.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Creaet a storage account
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
