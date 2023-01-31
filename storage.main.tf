da909a67237d7b8c183de60224869451e29bb4d7
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2023-01-31 14:13:03 +0530

    Update storage.main.tf

--- storage.main.tf
+++ storage.main.tf
@@ -1,3 +1,23 @@
+variable "clientid" {
+  type        = string  
+  default     = "ce1425fc-f3d9-4afa-a1df-9786245f41fc"
+}
+
+variable "clientsecret" {
+  type        = string
+  default = "jA48Q~hG6j07H3Rc6fPptUshMcr3l3qBb2AEebKD"
+}
+
+variable "tenantid" {
+  type        = string  
+  default     = "459865f1-a8aa-450a-baec-8b47a9e5c904"
+}
+
+variable "subscriptionid" {
+  type        = string  
+  default     = "6e4924ab-b00c-468f-ae01-e5d33e8786f8"
+}
+
 # AzureRM provider version
 terraform {
   required_providers {
@@ -11,6 +31,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscriptionid
+  client_id       = var.clientid
+  client_secret   = var.clientsecret
+  tenant_id       = var.tenantid
 }
 
 # Create a resource group
